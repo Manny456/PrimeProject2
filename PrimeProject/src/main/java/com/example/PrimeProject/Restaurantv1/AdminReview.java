9c17b2fc2ccc2cfcc9190a1e1967944d649d6dea
Emmanuel <emmanuelnv01@gmail.com>
2023-12-17 00:16:12 +0000

    Finally removed IsReviewBoolean and created a new one in an AdminReview Class

---
+++ PrimeProject/src/main/java/com/example/PrimeProject/Restaurantv1/AdminReview.java
@@ -0,0 +1,13 @@
+package com.example.PrimeProject.Restaurantv1;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Getter
+@Setter
+public class AdminReview {
+
+    public Boolean isReviewAccepted;
+}
